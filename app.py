fb1b746436e9ebf956e686478d60c2eaf4b4c432
Hermann Weiss <56585320+HRMNN@users.noreply.github.com>
2022-06-23 10:07:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+import dash
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+################################################################################
+# APP INITIALIZATION
+################################################################################
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# this is needed by gunicorn command in procfile
+server = app.server
+
 import matplotlib.pyplot as plt
 x = [1,2,3,4,5,6,7,8,9]
 y = [7,3,7,4,3,8,4,8,3]
